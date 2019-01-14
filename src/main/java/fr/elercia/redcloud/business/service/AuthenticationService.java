dd8f1273e62d652ee8bfc35a61eb63b79101c3b5
Elercia <rabierv@hotmail.fr>
2019-01-14 22:32:18 +0100

    fix

--- src/main/java/fr/elercia/redcloud/business/service/AuthenticationService.java
+++ src/main/java/fr/elercia/redcloud/business/service/AuthenticationService.java
@@ -55,6 +55,7 @@ public class AuthenticationService {
         }
 
         if(!isValidToken(token)){
+            tokenRepository.delete(token);
             throw new TokenNotFoundException();
         }
 
