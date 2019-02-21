f59737b2be46c825d746f11b25e78512deab5f28
Elercia <rabierv@hotmail.fr>
2019-02-21 23:41:55 +0100

    Fix error in populate db

--- server/src/test/java/fr/elercia/redcloud/PopulateDB.java
+++ server/src/test/java/fr/elercia/redcloud/PopulateDB.java
@@ -3,6 +3,7 @@ package fr.elercia.redcloud;
 import com.google.common.collect.Iterables;
 import fr.elercia.redcloud.api.dto.entity.CreateUserDto;
 import fr.elercia.redcloud.business.entity.DynamicConfig;
+import fr.elercia.redcloud.business.entity.Environment;
 import fr.elercia.redcloud.business.service.PasswordEncoder;
 import fr.elercia.redcloud.business.service.UserService;
 import fr.elercia.redcloud.dao.repository.DynamicConfigRepository;
@@ -29,7 +30,7 @@ public class PopulateDB {
     void test() throws InvalidUserCreationException {
 
         DynamicConfig pathConfig = new DynamicConfig(DynamicConfig.DynamicConfigName.STORAGE_PATH, "C:/Users/rabie/Documents/redCloud/server/content");
-        DynamicConfig envConfig = new DynamicConfig(DynamicConfig.DynamicConfigName.ENVIRONMENT, "C:/Users/rabie/Documents/redCloud/server/content");
+        DynamicConfig envConfig = new DynamicConfig(DynamicConfig.DynamicConfigName.ENVIRONMENT, Environment.DEV.toString());
         dynamicConfigRepository.saveAll(Arrays.asList(pathConfig, envConfig));
 
         CreateUserDto createUserDto = new CreateUserDto("testUser1", "password");
