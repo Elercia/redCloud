95f1616f494ea3b5f79d8679639adc6e18609a2c
elercia <rabierv@hotmail.fr>
2019-02-22 19:02:51 +0100

    Update populate db script to add admin

--- server/src/test/java/fr/elercia/redcloud/PopulateDB.java
+++ server/src/test/java/fr/elercia/redcloud/PopulateDB.java
@@ -1,12 +1,13 @@
 package fr.elercia.redcloud;
 
-import com.google.common.collect.Iterables;
 import fr.elercia.redcloud.api.dto.entity.CreateUserDto;
 import fr.elercia.redcloud.business.entity.DynamicConfig;
 import fr.elercia.redcloud.business.entity.Environment;
-import fr.elercia.redcloud.business.service.PasswordEncoder;
+import fr.elercia.redcloud.business.entity.User;
+import fr.elercia.redcloud.business.entity.UserType;
 import fr.elercia.redcloud.business.service.UserService;
 import fr.elercia.redcloud.dao.repository.DynamicConfigRepository;
+import fr.elercia.redcloud.dao.repository.UserRepository;
 import fr.elercia.redcloud.exceptions.InvalidUserCreationException;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
-import java.util.UUID;
 
 @Disabled
 @SpringBootTest
@@ -22,6 +22,9 @@ public class PopulateDB {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     DynamicConfigRepository dynamicConfigRepository;
@@ -35,5 +38,11 @@ public class PopulateDB {
 
         CreateUserDto createUserDto = new CreateUserDto("testUser1", "password");
         userService.createUser(createUserDto);
+
+        CreateUserDto createAdminUserDto = new CreateUserDto("admin", "password");
+        User adminUser = userService.createUser(createAdminUserDto);
+
+        adminUser.setUserType(UserType.ADMIN);
+        userRepository.save(adminUser);
     }
 }
